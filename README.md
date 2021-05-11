9e6dc25f8e3016cc778ae28aa379326a1884e8f3
avermeulen <andre.vermeulen@gmail.com>
2021-05-11 16:47:43 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# try-gh-actions
+
+[![Node.js CI](https://github.com/codex-academy/try-gh-actions/actions/workflows/node.js.yml/badge.svg)](https://github.com/codex-academy/try-gh-actions/actions/workflows/node.js.yml)
---
+++ test/hello.test.js
@@ -0,0 +1,14 @@
+const hello = require('../hello');
+const assert = require('assert');
+
+describe('Test hello', function() {
+
+	it('should pass', function() {
+		assert.equal('Hello, Andre', hello('Andre'));
+	})
+	
+	it('should not pass', function() {
+		assert.equal('Hello, Daniel', hello('Daniel'));
+	});
+
+});
